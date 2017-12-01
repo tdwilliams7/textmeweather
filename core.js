bd0bf51c3974d2d6a506863ca2338c8df9f704c9
Troy Williams <troywilliams@Troys-MacBook-Pro.local>
2017-12-01 13:17:46 -0600

    add scheduler to run at 7:30 in the morning.

--- core.js
+++ core.js
@@ -1,42 +1,31 @@
 const twilio = require('twilio');
 const axios = require('axios');
-var client = new twilio('AC2251f514b4afb18ab44d23409de9a950', '4854ff3befdd2fd60367fdf07d3280de');
+const schedule = require('node-schedule');
+const client = new twilio('AC2251f514b4afb18ab44d23409de9a950', '4854ff3befdd2fd60367fdf07d3280de');
 const weatherURL = "https://api.darksky.net/forecast/391d7c57b9412748b10093f94c3aac8e/44.9754800,-93.2696860";
 
-axios
-  .get(weatherURL)
-    .then(response =>{
-        let today = response.data.daily.data[0];
-        let high = Math.round(today.apparentTemperatureHigh);
-        let low = Math.round(today.apparentTemperatureLow);
-        let summary = today.summary;
-        let message = "Good Morning, today's outlook: " + summary + ' Low of ' + low + '. High of ' + high + '.';
-        console.log(message);
-        client.messages.create({
-                to: '+15074699316',
-                from: '+15076077627',
-                body: message
-            })
+//fixed location in Minneapolis.
+const getWeather = () => {
+  axios
+    .get(weatherURL) //return an array od weath from darksky
+    .then(response => {
+      let today = response.data.daily.data[0];
+      let high = Math.round(today.apparentTemperatureHigh);
+      let low = Math.round(today.apparentTemperatureLow);
+      let summary = today.summary;
+      let message = "Good Morning, today's outlook: " + summary + ' Low of ' + low + '. High of ' + high + '.';
+      console.log(message);
+      client.messages.create({ //client is twilio.
+        to: '+15074699316',
+        from: '+15076077627',
+        body: message
+      })
     })
     .catch(error => {
-    console.log(error);
-  });
-
-
-
-
-
-// ()
-// var getWeather = function(){
-//     // http.get({
-//     //     url: "https://api.darksky.net/forecast/391d7c57b9412748b10093f94c3aac8e/44.9754800,-93.2696860",
-
-//     //     success: function(data){
-//     //         let today = data.daily.data[0];
-//     //         let high = Math.round(today.apparentTemperatureHigh);
-//     //         let low = Math.round(today.apparentTemperatureLow);
-//     //         let summary = today.summary;
-//     //         let message = "Good Morning, today's outlook " + summary + ' Low of ' + low + '. High of ' + high + '.';
-//     //         console.log(message);
-//         // }})};
-// getWeather
+      console.log(error);
+    });
+}
+//node schedule runs if it is 7:30 and 10 seconds in the morning.
+schedule.scheduleJob('10 30 7 * * *', function() {
+  getWeather();
+});
