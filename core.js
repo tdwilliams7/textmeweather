85e8900e98ebf9087698fb2a3e6302834056d99f
Troy Williams <troyDonwilliams@gmail.com>
2018-03-05 13:58:56 -0600

    updating moment. security alert

--- core.js
+++ core.js
@@ -7,13 +7,13 @@ const weatherURL = "https://api.darksky.net/forecast/391d7c57b9412748b10093f94c3
 //fixed location in Minneapolis.
 const getWeather = () => {
   axios
-    .get(weatherURL) //return an array od weath from darksky
+    .get(weatherURL) //return an array of weather from darksky
     .then(response => {
       let today = response.data.daily.data[0];
       let high = Math.round(today.apparentTemperatureHigh);
       let low = Math.round(today.apparentTemperatureLow);
       let summary = today.summary;
-      let message = "Good Morning, today's outlook: " + summary + ' Low of ' + low + '. High of ' + high + '.';
+      let message = `Good Morning, today's outlook: ${summary} Low of ${low}. High of ${high}.`;
       console.log(message);
       client.messages.create({ //client is twilio.
         to: '+15074699316',
@@ -25,7 +25,9 @@ const getWeather = () => {
       console.log(error);
     });
 }
+getWeather();
+
 //node schedule runs if it is 7:30 and 10 seconds in the morning.
-schedule.scheduleJob('10 30 7 * * *', function() {
-  getWeather();
-});
+// schedule.scheduleJob('10 40 7 * * *', function() {
+//   getWeather();
+// });
